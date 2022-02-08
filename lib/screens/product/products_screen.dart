2f5895edd24977d9962f66d8c1f7af4da079b66f
ahmedmohab96 <ahmdmhabhbyb@gmail.com>
2022-02-09 01:15:46 +0200

    https://github.com/ahmedmohab96/Furniture_app.git

---
+++ lib/screens/product/products_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_svg/svg.dart';
+import 'package:furniture_app/constants.dart';
+
+import 'components/body.dart';
+
+class ProductsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      backgroundColor: kPrimaryColor,
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      centerTitle: false,
+      title: Text('Dashboard'),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/notification.svg"),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
